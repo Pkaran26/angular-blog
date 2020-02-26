8ec5949875448a89e9612bc566619567037295fa
Prateek <kaushikprateek11@gmail.com>
2020-02-26 21:22:05 +0530

    method

--- src/app/adminlogin/adminlogin.component.ts
+++ src/app/adminlogin/adminlogin.component.ts
@@ -14,4 +14,8 @@ export class AdminloginComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onSubmit(){
+
+  }
+
 }
--- src/app/signup/signup.component.ts
+++ src/app/signup/signup.component.ts
@@ -15,4 +15,8 @@ export class SignupComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onSubmit(){
+
+  }
+
 }
