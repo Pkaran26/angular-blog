1f114ada9d4efc38259a84d1b5ada0da5736be56
Prateek <prateek.kaushik@graffersid.com>
2020-02-26 12:51:23 +0530

    1st

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,45 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { BloglistComponent } from './bloglist/bloglist.component';
+import { BlogdetailComponent } from './blogdetail/blogdetail.component';
 
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { UserComponent } from './user/user.component';
+import { CreateblogComponent } from './createblog/createblog.component';
+
+import { AdminComponent } from './admin/admin.component';
+import { AdminloginComponent } from './adminlogin/adminlogin.component';
+import { AdminsignupComponent } from './adminsignup/adminsignup.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/blog', pathMatch: 'full' },
+  { path: 'blog', component: BloglistComponent },
+  { path: 'blog/:id', component: BlogdetailComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'signup', component: SignupComponent },
+  { path: 'dashboard', component: UserComponent },
+  { path: 'newblog', component: CreateblogComponent },
+  { path: 'adminlogin', component: AdminloginComponent },
+  { path: 'adminsignup', component: AdminsignupComponent },
+  { path: 'admindashboard', component: AdminComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
+export const routingComponents =[
+  BloglistComponent,
+  BlogdetailComponent,
+  LoginComponent,
+  SignupComponent,
+  UserComponent,
+  CreateblogComponent,
+  AdminComponent,
+  AdminloginComponent,
+  AdminsignupComponent
+]
